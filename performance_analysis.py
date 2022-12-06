1896eefd879938c9b64d7d91146486f01aaa830c
Pompey21 <marko.mekjavic@gmail.com>
2022-12-06 13:32:17 +0100

    added testing file

---
+++ performance_analysis.py
@@ -0,0 +1,11 @@
+'''
+This is the file for stress testing our algorithm through various iterations.
+'''
+
+import numpy as np
+from math import exp
+import time
+from NQueens import NQueens
+
+average_duration = []
+
